cf29433bebcb2614fe459c70736c45a687ccc1b9
allfake <allfake24@gmail.com>
2014-03-30 17:00:26 +0700

    first init

---
+++ sendDataNotUse/sendData.ino
@@ -0,0 +1,37 @@
+/*
+  Example for different sending methods
+  
+  http://code.google.com/p/rc-switch/
+  
+  Need help? http://forum.ardumote.com
+*/
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  // Transmitter is connected to Arduino Pin #10  
+  mySwitch.enableTransmit(10);
+
+  // Optional set pulse length.
+  // mySwitch.setPulseLength(320);
+  
+  // Optional set protocol (default is 1, will work for most outlets)
+  // mySwitch.setProtocol(2);
+  
+  // Optional set number of transmission repetitions.
+  mySwitch.setRepeatTransmit(15);
+  
+}
+
+void loop() {
+
+
+  /* Same switch as above, but using binary code */
+  mySwitch.send("000000000001010100010001");
+  delay(1000);  
+}
