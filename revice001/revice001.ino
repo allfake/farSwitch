57186d56c0c92eb07d3284a2e7024c1504240074
Nat <nat.wrw@gmail.com>
2014-05-05 14:19:06 +0700

    Improve

--- revice001/revice001.ino
+++ revice001/revice001.ino
@@ -8,37 +8,47 @@
 
 #include <RCSwitch.h>
 
+#define OFF_CODE 500
+#define ON_CODE  501
+
 RCSwitch mySwitch = RCSwitch();
 int swtichPin = 13;
+int state, command_value;
+
 void setup() {
   Serial.begin(9600);
+  Serial.println("-BEGIN-");
   mySwitch.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
   
   pinMode(swtichPin, OUTPUT);
-  
-  pinMode(8, OUTPUT);
-  digitalWrite(8, HIGH);
-  digitalWrite(swtichPin, HIGH);
+  state = LOW;
+  digitalWrite(swtichPin, state);
 }
 
 void loop() {
   if (mySwitch.available()) {
-    
-    int value = mySwitch.getReceivedValue();
-    
-    if (value == 0) {
+    command_value = mySwitch.getReceivedValue();
+    Serial.print("GOT ");
+    Serial.println(command_value);
+  
+    if (command_value == 0) {
       Serial.print("Unknown encoding");
     } 
-    else if (mySwitch.getReceivedValue() == 10) {
-      digitalWrite(swtichPin, LOW); 
-      Serial.println("0");
-    }
-    else if (mySwitch.getReceivedValue() == 11) {
-      digitalWrite(swtichPin, HIGH); 
-      Serial.println("1");
-    }
-    else {   
+    else if (state == HIGH && command_value == OFF_CODE) {
+      state = LOW;
+      digitalWrite(swtichPin, state);
 
     }
+    else if (state == LOW && command_value == ON_CODE) {
+      state = HIGH;
+      digitalWrite(swtichPin, state); 
+    }
+    else if (command_value != ON_CODE && command_value != OFF_CODE) {
+      Serial.println("INVALID CODE");
+    }
+    else {   
+      Serial.println("NOPE");
+    }
+    delay(500);
   }
 }
